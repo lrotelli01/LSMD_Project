43a68b0167592e887f82d72600bf9b113044ccf3
Omar <o.sfar@studenti.unipi.it>
2026-01-27 11:45:46 +0100

    Add PropertyNode and UserNode classes for Neo4j integration
    - Introduced PropertyNode class to represent properties in the Neo4j graph with fields for propertyId, name, and city.
    - Introduced UserNode class to represent users in the Neo4j graph with fields for username, mongoId, role, and name.
    - Both classes utilize Lombok annotations for boilerplate code reduction and Spring Data Neo4j annotations for schema mapping.

---
+++ largeB&B/largebeb/src/main/java/largebeb/model/UserNode.java
@@ -0,0 +1,28 @@
+package largebeb.model.graph;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+
+@Node("User")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserNode {
+
+    @Id
+    private String username; // We use username as the primary key in the Graph
+
+    @Property("mongoId")
+    private String mongoId; // This links this Node to the MongoDB Document
+
+    private String role; // Useful for graph queries (e.g. "Find all Managers...")
+
+    private String name; // for easier visualization without querying Mongo
+    
+}
