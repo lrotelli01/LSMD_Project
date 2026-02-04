bdd1e9582b01a16f15b3d10460203db571983536
agrillo6-prog <a.grillo6@studenti.unipi.it>
2026-02-04 01:44:13 +0100

    Update PointOfInterest.java

--- largeB&B/largebeb/src/main/java/largebeb/model/PointOfInterest.java
+++ largeB&B/largebeb/src/main/java/largebeb/model/PointOfInterest.java
@@ -4,11 +4,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PointOfInterest {
+    @Field("id")
     private String id;
     private String name;
     private String category; // Museums, Parks, historical, restaurant, bar, etc.
